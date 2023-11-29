67178fdc9035295257fea79370eb5ed01b512ffb
akashsaraf <akash.saraf444@gmail.com>
2023-11-29 19:27:45 +0530

    init commit

---
+++ src/test/java/com/keycloak/keycloakspringboot/KeycloakSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keycloak.keycloakspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeycloakSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
