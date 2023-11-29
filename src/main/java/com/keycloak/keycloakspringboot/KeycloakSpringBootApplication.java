da57b5c370440a00b85466b1b4f74edbfd4bb280
akashsaraf <akash.saraf444@gmail.com>
2023-11-29 19:31:01 +0530

    init commit 2

--- src/main/java/com/keycloak/keycloakspringboot/KeycloakSpringBootApplication.java
+++ src/main/java/com/keycloak/keycloakspringboot/KeycloakSpringBootApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class KeycloakSpringBootApplication {
 
 	public static void main(String[] args) {
+		//TESTING COmmit
 		SpringApplication.run(KeycloakSpringBootApplication.class, args);
 	}
 
